3bb9bdd379fcb82b6d1f450fd60c1a9a52a074f9
Ayan <ayan.mani.tripathi@gmail.com>
2025-08-06 02:52:35 +0530

    feat: created smart contract

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,  // This helps with stack too deep errors
+    },
+  },
+  // ... rest of your config
+};
